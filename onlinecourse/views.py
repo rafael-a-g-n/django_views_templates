4079feda9c9753e0227145427c9b9895857e6dfa
rafael-a-g-n <ragn.ryzen@gmail.com>
2025-12-15 00:16:05 +0000

    add popular course list view and enrollment functionality

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -7,4 +7,26 @@ from django.views import generic
 from django.http import Http404
 
 # Create your views here.
+def popular_course_list(request):
+    context = {}
+    # If the request method is GET
+    if request.method == 'GET':
+        # Using the objects model manage to read all course list
+        # and sort them by total_enrollment descending
+        course_list = Course.objects.order_by('-total_enrollment')[:10]
+        # Append the course list as an entry of context dict
+        context['course_list'] = course_list
+        return render(request, 'onlinecourse/course_list.html', context)
+
+def enroll(request, course_id):
+    # If request method is POST
+    if request.method == 'POST':
+        # First try to read the course object
+        # If could be found, raise a 404 exception
+        course = get_object_or_404(Course, pk=course_id)
+        # Increase the enrollment by 1
+        course.total_enrollment += 1
+        course.save()
+        # Return a HTTP response redirecting user to course list view
+        return HttpResponseRedirect(reverse(viewname='onlinecourse:popular_course_list'))
 
